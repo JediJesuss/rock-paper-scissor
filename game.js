ca5374c414d00dad77be880d9a49676fe307096d
CristainG <cris569710@gmail.com>
2023-01-31 07:53:33 -0800

    Sucessfully able to play one round of game

--- game.js
+++ game.js
@@ -1,6 +1,19 @@
 function getComputerChoice() {
    
-    var choices = ["Rock" , "Paper" , "Sissors"];
+    var choices = ["rock" , "paper" , "sissors"];
     var result = choices[Math.floor(Math.random()*choices.length)];
     return result;
+}
+
+function playRound(player){
+    let computer = getComputerChoice();
+    let result = "";
+
+    if(player == "rock"){
+        if(computer == "paper"){
+            return result = "You lose";
+        }else if(computer == "sissors"){
+            return result = "You win";
+        } else return result = "Nobody wins";
+    }
 }
