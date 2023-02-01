b13f1ce2c5b4ce55fc87754d7cfbe37afcfc8f44
CristainG <cris569710@gmail.com>
2023-02-01 05:41:59 -0800

    Game is now able to track score and tell the winner overall.

--- game.js
+++ game.js
@@ -5,15 +5,56 @@ function getComputerChoice() {
     return result;
 }
 
-function playRound(player){
-    let computer = getComputerChoice();
-    let result = "";
+function getPlayerChoice(){
+    var playerSelection = prompt("Please Pick");
+        playerSelection = playerSelection.toLowerCase();
 
-    if(player == "rock"){
-        if(computer == "paper"){
-            return result = "You lose";
-        }else if(computer == "sissors"){
-            return result = "You win";
-        } else return result = "Nobody wins";
+    return playerSelection;
+}
+
+var playerPoints = 0;
+var computerPoints = 0;
+
+function playRound(){
+    var  playerChoice = getPlayerChoice();
+    var computerChoice = getComputerChoice();
+
+    let player = "You win!";
+    let computer  = "You Lose!";
+
+    if(playerChoice == "rock" && computerChoice == "paper"){
+        alert(computer);
+        computerPoints++;
+    }else if(playerChoice == "rock" && computerChoice=="sissors"){
+        alert(player);
+        playerPoints++;
+    }else if(playerChoice == "paper" && computerChoice=="rock"){
+        alert(player);
+        playerPoints++;
+    }else if(playerChoice == "paper" && computerChoice=="sissors"){
+        alert(computer);
+        computerPoints++;
+    }else if(playerChoice == "sissors" && computerChoice=="rock"){
+        alert(computer);
+        computerPoints++;
+    }else if(playerChoice == "sissors" && computerChoice=="paper"){
+        alert(player);
+        playerPoints++;
+    } else alert("It's a Draw!");
+
+}
+
+function game(){
+    for(let i = 0; i <5; i++){
+        var round = playRound();
+        if(i == 4){
+            if(playerPoints > computerPoints){
+                alert("You Win! ------- Final Score: Player: " +playerPoints+" Computer: " +computerPoints);
+            }else if (playerPoints == computerPoints){
+                alert("Draw!  ------- Final Score: Player: " +playerPoints+" Computer: " +computerPoints);
+            }else alert("You Lose! ------- Final Score: Player: " +playerPoints+" Computer: " +computerPoints);
+        } else {
+            alert("Current Score: Player: " + playerPoints + " Computer: " + computerPoints)
+        }
     }
 }
